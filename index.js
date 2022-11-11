e990ca2193e30ceefbd9ba52b223af717c4d10a4
Nayoung Jung <nang518@icloud.com>
2022-11-11 18:03:30 +0900

    apply naver API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const app = express()
 const port = 8080
 
@@ -7,7 +8,51 @@ app.get('/', (req, res) => {
 })
 
 app.get('/maybe', (req, res) => {
-    console.log("요청이 왔어요~")
+    const url = "https://openapi.naver.com/v1/datalab/search"
+    const headers = {
+        'Content-Type' : 'application/json',
+        'X-Naver-Client-Id' : "9mpUE8l_3rIhmgeeWQCf",
+        'X-Naver-Client-Secret' : "exT2k10h1i",
+    };
+
+    const request_body = {
+        "startDate": "2017-01-01",
+        "endDate": "2017-04-30",
+        "timeUnit": "month",
+        "keywordGroups": [
+            {
+                "groupName": "한글",
+                "keywords": [
+                    "한글",
+                    "korean"
+                ]
+            },
+            {
+                "groupName": "영어",
+                "keywords": [
+                    "영어",
+                    "english"
+                ]
+            }
+        ],
+        "device": "pc",
+        "ages": [
+            "1",
+            "2"
+        ],
+        "gender": "f"
+    };
+
+    fetch(url, {
+        method : "POST",
+        headers : headers,
+        body : JSON.stringify(request_body),
+    })
+        .then((response) => response.json())
+        .then((data)=> res.json({
+            "success": true,
+            data: data
+        }));
 })
 
 app.listen(port, () => {
